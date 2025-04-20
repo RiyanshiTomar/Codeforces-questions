48503ee5c478a4e132d12e4e278dc70d557f0ea8
Riyanshi Tomar <r0500482@gmail.com>
2025-04-20 14:06:20 +0530

    20-april-2025

--- Word.c
+++ Word.c
@@ -4,9 +4,26 @@
 int main(){
     char str[100];
     scanf("%s", str);
+    int countUpper = 0;
+    int countLower = 0;
     for(int i = 0; str[i] != '\0'; ++i){
-        str[i] = tolower(str[i]);
+        if(str[i] < 91){
+            countUpper++;
+        }
+        else{
+            countLower++;
+        }
        
+    }
+    if(countLower >= countUpper){
+        for(int i = 0; str[i] != '\0'; ++i){
+            str[i] = tolower(str[i]);
+        }
+    }
+    else{
+        for(int i = 0; str[i] != '\0'; ++i){
+            str[i] = toupper(str[i]);
+        }
     }
     printf("%s", str);
     return 0;
